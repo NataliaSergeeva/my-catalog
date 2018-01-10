2bd741288e5949781017cda7826ff323acfd3d06
natalia <nata_tory@mail.ru>
2018-01-10 22:54:54 +0700

    second commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '@2iz&uiu@-_5oqkhpbkldo*8iyth^^$o972nw1!hkv-z=g#oy_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'newnews.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'sergeeva.pythonanywhere.com']
 
 
 # Application definition
