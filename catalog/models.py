b92e5095757899f2b8cf29af65af486b1ae97c8b
natalia <nata_tory@mail.ru>
2018-01-12 20:35:27 +0700

    Added the creation and moderating of comments.

--- catalog/models.py
+++ catalog/models.py
@@ -17,3 +17,21 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
+        
+
+class Comment(models.Model):
+    post = models.ForeignKey('catalog.Post', related_name='comments')
+    author = models.CharField(max_length=20)
+    text = models.CharField(max_length=300)
+    created_date = models.DateTimeField(default=timezone.now)
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.text
