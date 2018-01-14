414336889c5bc2ab0368cfb384f4acaa344ee5f0
natalia <nata_tory@mail.ru>
2018-01-14 22:36:50 +0700

    Added logout, images and files.

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '@2iz&uiu@-_5oqkhpbkldo*8iyth^^$o972nw1!hkv-z=g#oy_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'sergeeva.pythonanywhere.com', '/static/']
+ALLOWED_HOSTS = ['127.0.0.1', 'sergeeva.pythonanywhere.com', '/static/', '/media/']
 
 
 # Application definition
